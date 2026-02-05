004e4e659506d82676f411927eb0b41ed37e4b6c
asdflkj169 <asdflkj169@gmail.com>
2026-02-05 12:15:58 +0000

    コメント追加とcdn削除

--- src/routes/dev/test-hand-landmarker.tsx
+++ src/routes/dev/test-hand-landmarker.tsx
@@ -45,12 +45,10 @@ function TestHandLandmarkerComponent() {
     let rId: number;
     let canvasCtx: CanvasRenderingContext2D | null = null;
     const createHandLandmarker = async () => {
-      const vision = await FilesetResolver.forVisionTasks(
-        "https://cdn.jsdelivr.net/npm/@mediapipe/tasks-vision@0.10.0/wasm",
-      );
+      const vision = await FilesetResolver.forVisionTasks("/mediapipe/wasm");
       handLandmarker = await HandLandmarker.createFromOptions(vision, {
         baseOptions: {
-          modelAssetPath: `https://storage.googleapis.com/mediapipe-models/hand_landmarker/hand_landmarker/float16/1/hand_landmarker.task`,
+          modelAssetPath: `/mediapipe/hand_landmarker.task`,
           delegate: "GPU",
         },
         runningMode: "VIDEO",
